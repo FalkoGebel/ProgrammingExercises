872014869157e18157d260004ff5acd50fa0198b
Falko Gebel <falko-gebel@live.de>
2025-07-07 23:06:34 +0200

    Created library project, test project and WPF project for display of solutions and worked on basic exercise number 1

---
+++ SolutionsLibraryTests/BasicExercisesTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using SolutionsLibrary;
+
+namespace SolutionsLibraryTests
+{
+    [TestClass]
+    public sealed class BasicExercisesTests
+    {
+        [DataTestMethod]
+        [DataRow("Alexandra Abramov")]
+        [DataRow("Yolanda Walter Bennish")]
+        [DataRow("Marius Cotandrum")]
+        public void Exercise_01_input_name_return_correct_output(string name)
+        {
+            string expected = $"Hello: {name}";
+
+            string result = BasicExercises.GetHelloAndName(name);
+
+            result.Should().Be(expected);
+        }
+    }
+}
